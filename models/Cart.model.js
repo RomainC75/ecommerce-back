34b9b3b516c99af6c505c12f1edb0786a0e53e39
RomainC75 <rom.chenard@gmail.com>
2022-08-01 21:29:05 +0200

    minor changes + stop from adding _id key in subdocuments

--- models/Cart.model.js
+++ models/Cart.model.js
@@ -1,24 +1,19 @@
 const { Schema, SchemaTypes, model } = require("mongoose");
 
+const productToOrder = new Schema({
+    productId: { 
+      type: SchemaTypes.ObjectId, 
+      ref: "Product"
+   },
+    quantity:{type: Number, required:true}
+},{_id:false})
+
 const cartSchema = new Schema({
   userId: {
     type: SchemaTypes.ObjectId,
     ref: "User",
   },
-  products: [
-    {
-      productId: { 
-        type: SchemaTypes.ObjectId, 
-        ref: "Product"
-     },
-      quantity:{type: Number, required:true}
-    },
-  ],
-  status: {
-    type: SchemaTypes.String,
-    enum: ["Pending", "Payed", "Shipped", "Received"],
-    default: "Pending"
-  },
+  products: [productToOrder]
 },{
   timestamps:true
 });
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -5,8 +5,8 @@ const authentication = require('../middlewares/authentication.mid')
 router.get('/',authentication, async(req,res,next)=>{
     console.log(req.user)
     try {
-        const ans = await Cart.find({userId:req.user._id})
-        res.status(200).json(ans[0])
+        const ans = await Cart.findOne({userId:req.user._id})
+        res.status(200).json(ans)
     } catch (error) {
         next(error)
     }
@@ -15,18 +15,19 @@ router.get('/',authentication, async(req,res,next)=>{
 router.post('/',authentication, async(req,res,next)=>{
     try {
         const ans = await Cart.find({userId:req.user._id})
+        console.log('BODY : ',req.body)
         if(ans.length>0){
             res.status(400).json({message: 'cart already exists !'})
             return
         }
         //test values inside the request
         if(Array.isArray(req.body)){
-            req.body.forEach(val=>{
-              if(!'productId' in val || !'quantity' in val){
-                res.status(404).json({message:'data format problem (object)'})
-                return
-              }  
-            })
+            // req.body.forEach(val=>{
+            //   if(!'productId' in val || !'quantity' in val){
+            //     res.status(404).json({message:'data format problem (object)'})
+            //     return
+            //   }  
+            // })
             const ans = await Cart.create({
                 userId:req.user._id,
                 products:req.body
@@ -43,7 +44,23 @@ router.post('/',authentication, async(req,res,next)=>{
 
 router.patch('/',authentication, async (req,res,next)=>{
     try {
-        
+        const ans = await Cart.find({userId:req.user._id})
+        if(ans.length===0){
+            res.json(404).json({message:"cart doesn\'t exist !"})
+            return
+        }
+        const ansUpdate = await Cart.findOneAndUpdate({userId:req.user._id},{products:req.body},{new:true})
+        res.status(200).json(ansUpdate)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.delete('/', authentication, async (req,res,next)=>{
+    try {
+        const ans = await Cart.findOneAndDelete({userId:req.user._id})
+        console.log('delete : ',ans )
+        res.status(400).json(ans)
     } catch (error) {
         next(error)
     }
