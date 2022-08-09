bf6b65f51acde53a6aff977b719a9884c2d7a973
RomainC75 <rom.chenard@gmail.com>
2022-08-09 17:07:52 +0200

    route user : post

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const router=require('express').Router()
 const authentication = require('../middlewares/authentication.mid')
 const User = require("../models/User.model")
+
 router.get('/', authentication, async(req,res,next)=>{
     try {
         const foundUser = await User.findOne({_id:req.user._id}).select({emailValidationCode:0, password:0})
@@ -9,6 +10,21 @@ router.get('/', authentication, async(req,res,next)=>{
             return
         }
         res.status(200).json(foundUser)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.patch('/', authentication, async(req,res,next)=>{
+    try {
+        const foundUser = await User.findOne({_id:req.user._id})
+        if(!foundUser){
+            res.status(404).json({message:"user not found !"})
+            return
+        }
+        const ans = await User.findOneAndUpdate({_id:req.user._id},req.body,{new:true}).select({emailValidationCode:0, password:0})
+        console.log('ANS : ',ans)
+        res.status(200).json(ans)
     } catch (error) {
         next(error)
     }
