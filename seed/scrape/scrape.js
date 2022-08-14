7ea672111d8bbeef700a0469b63887720fae1b45
RomainC75 <rom.chenard@gmail.com>
2022-08-14 11:15:38 +0200

    scraping : get other caracteristics & write results in a JSON file

--- seed/scrape/scrape.js
+++ seed/scrape/scrape.js
@@ -11,8 +11,10 @@
 //     weight
 
 const puppeteer = require('puppeteer')
+const dataJSON = require('./data.json')
+const fs = require('fs')
 
-const site_url="https://www.greenweez.com/gaia-faux-gras-125g-p77235?objectID=77235_0_0_1107"
+const site_url="https://www.greenweez.com/p-tits-dessous-lot-de-300-voiles-de-protection-jetables-couches-lavables-13x30-p126516?objectID=126516_0_3930_8376&queryID=221b1682713cee02fc49590abd186921"
 
 const scrapePage = (url) =>{
     return new Promise( async(resolve, reject)=>{
@@ -39,33 +41,49 @@ const scrapePage = (url) =>{
 
             console.log('-->getting data ...')
             const data = await page.evaluate(()=>{
+                const catEl = Array.from(document.querySelectorAll('#fil_ariane li a'))
+                const categories = catEl.map(categorie=>categorie.textContent.replace(/[\n\t]/gm,'')).filter(category=>category!=="Accueil")
+                console.log('categories', categories)
+
                 const brand = document.querySelector('aside > div > div > div > a').textContent
                 console.log('brand :',brand)
-                const name = document.querySelector('aside > div > div h1').textContent
+                const name = document.querySelector('aside > div > div h1').textContent.replace(/[\n\t]/gm,'')
                 console.log('name : ',name)
-                const price=document.querySelector('#fp_col_price span').textContent.replace('€','').replace(',','.')
+                const price=document.querySelector('#fp_col_price span').textContent.replace(/[€\s]/gm,'').replace(',','.')
                 console.log('price : ',parseFloat(price))
 
                 const image = document.querySelector('#mirakl-main-slider img').getAttribute('src')
                 console.log('image : ',image)
 
-                // const accordion = document.querySelector('#accordion_fp')
-                // console.log('collapseOne',collapseOne)
-
-                // const details = accordion.querySelectorAll('.collapse.show')
-                // console.log('details : ',details)
-                //#product_description
+                const images = Array.from(document.querySelectorAll('.lp_miniatures a')).map(el=>el.getAttribute('data-img-small')).map(addr=>addr.replace('/50/','/600/'))
+                console.log('images : ',images)
+                
                 const description = document.querySelector('#product_description').textContent
                 console.log('description : ',description)
-                //.caracteristiques_fp  
-                    //.sous_titre>Caractéristiques
+
                 const caracteristics = document.querySelector('.caracteristiques_fp')
                 console.log('caracteristics',caracteristics)
                 const caracP = Array.from(caracteristics.querySelectorAll('p')).map(p=>p.textContent)
                 console.log('-------------------------')
                 console.log('CARACP',caracP)
-            })
 
+                const caracteristics2=document.querySelector('.caracteristiques_fp').textContent
+                // const caracteristics2=Array.from(document.querySelectorAll('.caracteristiques_fp > *')).map(el=>el.textContent)
+
+                return {
+                    categories,
+                    brand,
+                    name,
+                    price,
+                    image,
+                    images,
+                    description,
+                    caracterisctics:caracP,
+                    caracteristics2
+                }
+            })
+            console.log('-->DATA : ',data)
+            resolve(data)
 
         } catch (error) {
             console.log('-->ERROR  : ',error)
@@ -74,4 +92,20 @@ const scrapePage = (url) =>{
     } )
 } 
 
-scrapePage(site_url)
+scrapePage(site_url).then(
+  (result) => {
+    dataJSON.push(result);
+    fs.writeFile(
+      "data.json",
+      JSON.stringify(dataJSON),
+      "utf8",
+      function (err) {
+        if (err) {
+          console.log("An error occured while writing JSON Object to File.");
+          return console.log(err);
+        }
+        console.log("JSON file has been saved.");
+      }
+    );
+  }
+);
