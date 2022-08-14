0e2cae8c63f54fce2e9e0d16bd1cba6f5003c755
RomainC75 <rom.chenard@gmail.com>
2022-08-14 19:22:47 +0200

    scrape site with puppeteer-cluster and get the product addresses

--- seed/scrape/scrape.js
+++ seed/scrape/scrape.js
@@ -13,22 +13,17 @@
 const puppeteer = require('puppeteer')
 const dataJSON = require('./data.json')
 const fs = require('fs')
+const { resolve } = require('path')
+const { clickCookiesButton } = require('./tools')
+const {Cluster} = require('puppeteer-cluster')
 
 const site_url="https://www.greenweez.com/p-tits-dessous-lot-de-300-voiles-de-protection-jetables-couches-lavables-13x30-p126516?objectID=126516_0_3930_8376&queryID=221b1682713cee02fc49590abd186921"
 
-const scrapePage = (url) =>{
+
+
+const scrapeProduct = (page, url) =>{
     return new Promise( async(resolve, reject)=>{
         try {
-            const browser = await puppeteer.launch({
-                headless:false,
-                slowmo:300,
-                devtools:true
-            })
-            const page = await browser.newPage()
-            await page.setViewport({
-                width:1200,
-                height:900
-            })
             await page.goto(url, {waitUntil:"networkidle2"})
             
             console.log('click cookies button')
@@ -92,20 +87,160 @@ const scrapePage = (url) =>{
     } )
 } 
 
-scrapePage(site_url).then(
-  (result) => {
-    dataJSON.push(result);
-    fs.writeFile(
-      "data.json",
-      JSON.stringify(dataJSON),
-      "utf8",
-      function (err) {
-        if (err) {
-          console.log("An error occured while writing JSON Object to File.");
-          return console.log(err);
+const getCategoriesUrl = () =>{
+    const url = "https://www.greenweez.com/"
+    return new Promise ( async (resolve, reject)=>{
+        try {
+            console.log('-->gettingcategories url')
+            const browser = await puppeteer.launch({
+                headless:false,
+                slowmo:300,
+                devtools:true
+            })
+            const page = await browser.newPage()
+            await page.setViewport({
+                width:1200,
+                height:900
+            })
+            await page.goto(url, {waitUntil:"networkidle2"})
+            console.log('click cookies button')
+
+            await page.waitForSelector('#onetrust-accept-btn-handler')
+            const buttons = await page.$x("//button[contains(., 'Tout accepter')]");
+            //   console.log(buttons)
+            if (buttons[1]) {
+                await buttons[1].click();
+              }
+            const categories = await page.evaluate(()=>{
+                return Array.from(document.querySelectorAll('.cat_level_2 a')).map(elA=>elA.getAttribute('href'))
+            })
+            console.log('---->got categories url')
+            await browser.close()
+            resolve(categories)
+        } catch (error) {
+            reject(error)
         }
-        console.log("JSON file has been saved.");
-      }
-    );
-  }
-);
+    })
+}
+
+
+
+const getProductsUrlFromCatPage = (page, url) =>{
+    return new Promise( async (resolve,reject)=>{
+        try {
+            const fullAddress = "https://www.greenweez.com/"+url
+            console.log('full address : ',fullAddress)
+            await page.goto(fullAddress,{waitUntil:"networkidle2"})
+            
+            clickCookiesButton(page)
+            await page.waitForSelector('#result-page__hits li')
+            const productList = await page.evaluate(()=>{
+                const lis = Array.from(document.querySelectorAll('#result-page__hits li'))
+                console.log('-->lis',lis)
+                const as = lis.map(li=>li.querySelector('a').getAttribute('href'))
+                console.log('as',as)
+                return as
+            })
+            console.log('productList : ',productList)
+            await page.close()
+            resolve(productList)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+const test = (page,url) =>{
+    return new Promise(async (resolve, reject)=>{
+        try {
+            console.log('-->',url)
+            page.on('console', msg => console.log(msg.text()));
+            await page.goto('http://www.google.fr',{waitUntil:'networkidle2'})
+            const res = await page.evaluate(()=>{
+                const h1 = document.querySelector('h1')
+                console.log("h1",h1)
+            })
+            resolve('hey',res)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+const getTheProductsUrlWithClusters = (categoriesArray) =>{
+    return new Promise(async (resolve,reject)=>{
+        try {
+            console.log('categoriesArray',categoriesArray)
+            const cluster = await Cluster.launch({ 
+                concurrency: Cluster.CONCURRENCY_PAGE, 
+                maxConcurrency: 3, 
+                monitor: false, 
+                timeout:10000, 
+                puppeteerOptions: { 
+                   headless: false, 
+                   slowMo: 250, 
+                   args: [`--window-size=${1680},${970}`,'--disable-dev-shm-usage'], 
+                }, 
+            });
+            cluster.on('taskerror', (err, data, willRetry) => {
+                if (willRetry) {
+                  console.warn(`Encountered an error while crawling ${data}. ${err.message}\nThis job will be retried`);
+                } else {
+                  console.error(`Failed to crawl ${data}: ${err.message}`);
+                }
+            });
+            await cluster.task(async ({ page, data: url }) => {
+                console.log('task')
+                // const productsUrl = await getProductsUrlFromCatPage(page,url)
+                const productsUrl=await getProductsUrlFromCatPage(page,url)
+                return productsUrl;
+            });
+
+            const infos = await Promise.all(Array.from(categoriesArray).map(categoryAddr=>{
+                console.log('categoryAddrr :',categoryAddr)
+                return cluster.execute(categoryAddr);
+            }))
+            await cluster.idle();
+            await cluster.close();
+            
+            resolve(infos)
+        } catch (error) {
+            reject('--->',error)
+        }
+    }) 
+}
+
+
+const scrapeSite = () =>{
+    return new Promise(async (resolve, reject)=>{
+        try {
+            const categoriesUrl = await getCategoriesUrl()
+            console.log('XXXXX got : ',categoriesUrl)
+            const productsUrl = await getTheProductsUrlWithClusters(categoriesUrl)
+            console.log('productsUrl',productsUrl)
+            resolve('x')
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+scrapeSite().then(data=>console.log('data')).catch(err=>console.log('err : ',err))
+
+// scrapePage(site_url).then(
+//   (result) => {
+//     dataJSON.push(result);
+//     fs.writeFile(
+//       "data.json",
+//       JSON.stringify(dataJSON),
+//       "utf8",
+//       function (err) {
+//         if (err) {
+//           console.log("An error occured while writing JSON Object to File.");
+//           return console.log(err);
+//         }
+//         console.log("JSON file has been saved.");
+//       }
+//     );
+//   }
+// );
---
+++ seed/scrape/tools.js
@@ -0,0 +1,62 @@
+const clickCookiesButton = async (page) =>{
+    console.log('click cookies button')
+    await page.waitForSelector('#onetrust-accept-btn-handler')
+    const buttons = await page.$x("//button[contains(., 'Tout accepter')]");
+    //   console.log(buttons)
+    if (buttons[1]) {
+        await buttons[1].click();
+        }
+}
+
+const scrollSection =  (page,sectionSelector) => {
+    return new Promise (async (resolve, reject)=>{
+        console.log('INSIDE');
+        page.on('console', msg => console.log(msg.text()));
+        await page.waitForSelector(sectionSelector);
+
+        /*
+        await page.evaluate( async selector => {
+            const scrollableSection = document.querySelector(selector).childNodes[0].childNodes[1];
+            console.log(scrollableSection);
+            scrollableSection.scrollTop = scrollableSection.offsetHeight;
+            
+            // await scrollableSection.waitForNetworkIdle();
+            // console.log('->li : ',scrollableSection.querySelectorAll('li'));
+        },sectionSelector);
+        */
+
+        const sectionToScroll = await page.$(sectionSelector);
+        console.log('section to scroll',sectionToScroll);
+        
+        let arrayToSaveRef = [];
+        let arrayToSaveNew = this.serializedObjectToArray(await sectionToScroll.$$eval('li',lis=>lis.map(li=>li.querySelector('a').getAttribute('href'))));
+        const listOfFriends = [];
+        console.log('arrayToSaveNew', arrayToSaveNew,typeof arrayToSaveNew, arrayToSaveNew.length);
+        let multiple=0;
+        do{
+            arrayToSaveRef=[...arrayToSaveNew];
+            await Promise.all([
+                await page.evaluate( selector => {
+                    const scrollableSection = document.querySelector(selector).childNodes[0].childNodes[1];
+                    //console.log(scrollableSection);
+                    //scrollableSection.scrollTop = scrollableSection.offsetHeight+(600*multiple);
+                    setTimeout(scrollableSection.scrollBy(0,800),2000);
+
+                },sectionSelector),
+                await page.waitForNetworkIdle(),
+
+            ]);
+            arrayToSaveNew = this.serializedObjectToArray(await sectionToScroll.$$eval('li',lis=>lis.map(li=>li.querySelector('a').getAttribute('href'))));
+            console.log('--> temps : ',arrayToSaveNew.length,'--> list of friends :',listOfFriends.length);
+            arrayToSaveNew.forEach(friend=>{
+                if(listOfFriends.includes(friend)){
+                    listOfFriends.push(friend);
+                }
+            });
+
+        }while(arrayToSaveRef[arrayToSaveRef.length-1] != arrayToSaveNew[arrayToSaveNew.length-1]);
+
+    })
+}
+
+module.exports={clickCookiesButton}
